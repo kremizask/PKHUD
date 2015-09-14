0a5624e543cba62bd832ac8d53c4eb0ad40d511f
Kostas Kremizas <kremizask@gmail.com>
2015-09-14 18:44:47 +0300

    swift 2.0 compatibility edits.

--- PKHUD/FrameView.swift
+++ PKHUD/FrameView.swift
@@ -16,7 +16,7 @@ internal class FrameView: UIView {
         commonInit()
     }
     
-    required init(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         commonInit()
     }
@@ -38,7 +38,7 @@ internal class FrameView: UIView {
         motionEffectsY.maximumRelativeValue = offset
         motionEffectsY.minimumRelativeValue = -offset
         
-        var group = UIMotionEffectGroup()
+        let group = UIMotionEffectGroup()
         group.motionEffects = [motionEffectsX, motionEffectsY]
         
         addMotionEffect(group)
--- PKHUD/PKHUDAssets.swift
+++ PKHUD/PKHUDAssets.swift
@@ -9,13 +9,17 @@
 import UIKit
 
 /// Provides a set of default assets, like images, that can be supplied to the PKHUD's contentViews.
-@objc public class PKHUDAssets {
+public class PKHUDAssets {
     public class var crossImage: UIImage { return PKHUDAssets.bundledImage(named: "cross") }
     public class var checkmarkImage: UIImage { return PKHUDAssets.bundledImage(named: "checkmark") }
     public class var progressImage: UIImage { return PKHUDAssets.bundledImage(named: "progress") }
     
     internal class func bundledImage(named name: String) -> UIImage {
         let bundle = NSBundle(forClass: PKHUDAssets.self)
-        return UIImage(named: name, inBundle:bundle, compatibleWithTraitCollection:nil)!
+        if #available(iOS 8.0, *) {
+            return UIImage(named: name, inBundle:bundle, compatibleWithTraitCollection:nil)!
+        } else {
+            return UIImage(named: name)!
+        }
     }
 }
